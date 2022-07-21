df576b5021c3e8acfa29ff44f66feda374c7705d
Avery Novick <averynov@yahoo.com>
2022-07-21 14:33:14 -0400

    updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,22 +2,11 @@ import ParkCard from '../components/ParkCard'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-const Home = () => {
-  const [parks, setParks] = useState([])
-
-  useEffect(() => {
-    const getParks = async () => {
-      const response = await axios.get('http://localhost:3001/parks')
-      setParks(response.parks)
-      // check back on this later
-    }
-    getParks()
-  }, [])
-
+const Home = (props) => {
   return (
     <div>
       <h1>Park</h1>
-      <ParkCard parks={parks} key={parks.id} />
+      <ParkCard parks={props.parks} />
     </div>
   )
 }
