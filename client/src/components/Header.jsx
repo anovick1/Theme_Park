7be3c91fb236e61bf15305e848bd71005d47ac26
Yangtsel Sherap <yangtsel.sherap@gmail.com>
2022-07-21 11:46:32 -0400

    updated files

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+
+  return (
+    <header>
+      <nav>
+        <Link to='/'>Home</Link>
+      </nav>
+      
+    </header>
+
+  )
+}
+
+export default Header
